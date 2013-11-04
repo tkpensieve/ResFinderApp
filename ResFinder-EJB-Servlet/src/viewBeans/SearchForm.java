2b08ed6a46d90eb82dba72c75811c61aead0183a
Karthikeyan Thangavel <tkchennai89@gmail.com>
2013-11-04 02:16:59 -0500

    Introduced JSF and findAll method in LocationService <Karthik>

--- ResFinder-EJB-Servlet/src/viewBeans/SearchForm.java
+++ ResFinder-EJB-Servlet/src/viewBeans/SearchForm.java
@@ -1,12 +1,38 @@
 package viewBeans;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import ejb.Entity.Location;
+import javax.ejb.EJB;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
 
+import ejb.Entity.Location;
+import ejb.Service.LocationService;
+import ejb.Service.RestaurantService;
+
+@ManagedBean
+@ApplicationScoped
 public class SearchForm {
+	@EJB
+	private LocationService locationService;
+	@EJB
+	private RestaurantService restaurantService;
+	
 	Location selectedLocation;
 	Map<String, Object> allLocations = new HashMap<String, Object>();
-
+	
+	public SearchForm(Map<String, Object> allLocations) {
+		this.allLocations = new HashMap<String,Object>();
+		ArrayList<Location> findAll = locationService.findAll();
+		for (Location location : findAll) {
+			allLocations.put(location.getName(), location.getId()); //label, value
+		}
+	}
+	
+	public void filter()
+	{
+//		restaurantService.findById(1);
+	}
 }
--- ResFinder-EJB-Session-Entity/ejbModule/ejb/Service/LocationService.java
+++ ResFinder-EJB-Session-Entity/ejbModule/ejb/Service/LocationService.java
@@ -2,11 +2,12 @@ package ejb.Service;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.*;
-
 
 import ejb.Entity.Location;
 
@@ -16,14 +17,10 @@ import ejb.Entity.Location;
 @Stateless
 @LocalBean
 public class LocationService {
-	 @PersistenceContext(unitName="resFinder-ejb-entities")
-		EntityManager em;
+	@PersistenceContext(unitName="resFinder-ejb-entities")
+	EntityManager em;
 
-    /**
-     * Default constructor. 
-     */
-   
-    public String createLocation(Location res) {
+	public String createLocation(Location res) {
 		try{
 		em.persist(res);
 		int beanID = res.getId();
@@ -34,29 +31,35 @@ public class LocationService {
 			StringWriter errors = new StringWriter();
 			e.printStackTrace(new PrintWriter(errors));
 			return errors.toString();
-		}
-		
-		
-			
+		}		
 	}
+	
+	public ArrayList<Location> findAll()
+	{
+		ArrayList<Location> allLocations = new ArrayList<Location>();
+		List<Location> results = em.createQuery("SELECT p FROM Person p").getResultList();
+		if(results.size() > 0)
+			allLocations.addAll(results);
+		return allLocations;
+	}
+	
 	public Location findById(int id)
 	{
 		Location u=em.find(Location.class,id);
 		return u;
 	}
+	
 	public void delete(int id)
 	{
 		Location u=em.find(Location.class,id);
 		em.remove(u);
 	}
+	
 	public void update(Location u)
 	{
-		
 		em.getTransaction().begin();
 		em.merge(u);
-		em.getTransaction().commit();
-		
-		
+		em.getTransaction().commit();	
 	}
 
 }
