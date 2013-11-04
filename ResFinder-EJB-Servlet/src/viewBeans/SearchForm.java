8f5c552395cfdee2bbfc2d50cf7dd205faa20f2f
Karthikeyan Thangavel <tkchennai89@gmail.com>
2013-11-03 22:14:50 -0500

    Introducing JSF <Karthik>

---
+++ ResFinder-EJB-Servlet/src/viewBeans/SearchForm.java
@@ -0,0 +1,12 @@
+package viewBeans;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import ejb.Entity.Location;
+
+public class SearchForm {
+	Location selectedLocation;
+	Map<String, Object> allLocations = new HashMap<String, Object>();
+
+}
