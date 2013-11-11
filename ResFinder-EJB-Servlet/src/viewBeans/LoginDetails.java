a11dfdf9fe8914e90c0506849db459655059ad06
Karthikeyan Thangavel <tkchennai89@gmail.com>
2013-11-10 22:25:36 -0500

    Logiin as manager <Karthik>

--- ResFinder-EJB-Servlet/src/viewBeans/LoginDetails.java
+++ ResFinder-EJB-Servlet/src/viewBeans/LoginDetails.java
@@ -3,7 +3,9 @@ import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import ejb.Entity.Manager;
 import ejb.Entity.User;
+import ejb.Service.ManagerService;
 import ejb.Service.UserService;
 
 @ManagedBean(name="LoginDetails")
@@ -11,12 +13,15 @@ import ejb.Service.UserService;
 public class LoginDetails {
 	@EJB
 	private UserService userService;
+	@EJB
+	private ManagerService managerService;
 	
 	String userId;
 	String password;
 	String registerEmail;
 	String loginMessage;
 	User loggedInUser;
+	boolean businessUser;
 	
 	public String getUserId() {
 		return userId;
@@ -50,8 +55,21 @@ public class LoginDetails {
 	public void setLoggedInUser(User loggedInUser) {
 		this.loggedInUser = loggedInUser;
 	}
+	public boolean isBusinessUser() {
+		return businessUser;
+	}
+	public void setBusinessUser(boolean businessUser) {
+		this.businessUser = businessUser;
+	}
 	public void login()	{
-		User user = userService.findById(userId);
+		User user = null;
+		if(businessUser){
+			Manager manager = managerService.findByUserid(userId);
+			user = manager.getUser();
+		}
+		else {
+			user = userService.findById(userId);
+		}
 		String message = "";
 		if(user.getPassword().toLowerCase().equals(password.toLowerCase())){
 			message =  "";
